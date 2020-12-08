507d8b238658544a235ca0b4b00b7915927e3729
Sarah Zaman <szaman@u.rochester.edu>
2020-12-08 04:01:03 -0500

    forms created for login and registration

--- hello.py
+++ hello.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-
+app.config['SECRET_KEY'] = 'gigliglgghv'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
 
 
 @app.errorhandler(404)
@@ -29,8 +36,25 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
     if form.validate_on_submit():
-        session['name'] = form.name.data
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+            if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+                 flash(f'You are logged in!', 'success')
+                 return redirect(url_for('index'))
+            else:
+                flash(f'Login unsuccessful. Please check username and password.', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
